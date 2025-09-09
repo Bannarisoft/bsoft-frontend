ef69148a5f4f5650a45f7fd3f58a597f856f7715
Bannarisoft <itsupport@bannarimills.co.in>
2025-09-09 09:28:43 +0530

    API Endpoints Changed

--- src/utils/apiDomains.ts
+++ src/utils/apiDomains.ts
@@ -1,15 +1,30 @@
-export const apiDomainConfig: Record<string, Record<string, string>> = {
-  "121.200.49.254": {
-    base: "http://121.200.49.254:30574",
-    fam: "http://121.200.49.254:30594",
-    main: "http://121.200.49.254:30593",
-    bg: "http://121.200.49.254:30511",
-  },
+// export const apiDomainConfig: Record<string, Record<string, string>> = {
+//   "121.200.49.254": {
+//     base: "http://121.200.49.254:30574",
+//     fam: "http://121.200.49.254:30594",
+//     main: "http://121.200.49.254:30593",
+//     bg: "http://121.200.49.254:30511",
+//   },
 
+//   localhost: {
+//     base: "http://121.200.49.254:30574",
+//     fam: "http://121.200.49.254:30594",
+//     main: "http://121.200.49.254:30593",
+//     bg: "http://121.200.49.254:30511",
+//   },
+// };
+
+export const apiDomainConfig: Record<string, Record<string, string>> = {
+  cluster: {
+    base: "http://usermanagement-service:5174",
+    fam: "http://fixedasset-service:5194",
+    main: "http://background-service:5293",
+    bg: "http://maintenance-service:5011",
+  },
   localhost: {
-    base: "http://121.200.49.254:30574",
-    fam: "http://121.200.49.254:30594",
-    main: "http://121.200.49.254:30593",
-    bg: "http://121.200.49.254:30511",
+    base: "http://localhost:5174",
+    fam: "http://localhost:5194",
+    main: "http://localhost:5293",
+    bg: "http://localhost:5011",
   },
 };
