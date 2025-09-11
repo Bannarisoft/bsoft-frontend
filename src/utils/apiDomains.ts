ce552a6a4ad7cf69790b9bd3c6ca67035f7fc272
Bannarisoft <itsupport@bannarimills.co.in>
2025-09-11 10:11:44 +0530

    Domain Name Updated

--- src/utils/apiDomains.ts
+++ src/utils/apiDomains.ts
@@ -1,8 +1,23 @@
+// export const apiDomainConfig: Record<string, Record<string, string>> = {
+//   cluster: {
+//     base: "http://usermanagement-service:5174/",
+//     fam: "http://fixedasset-service:5194/",
+//     main: "http://background-service:5293/",
+//     bg: "http://maintenance-service:5011/",
+//   },
+// };
+
 export const apiDomainConfig: Record<string, Record<string, string>> = {
-  cluster: {
-    base: "http://usermanagement-service:5174/",
-    fam: "http://fixedasset-service:5194/",
-    main: "http://background-service:5293/",
-    bg: "http://maintenance-service:5011/",
+  "121.200.49.254": {
+    base: "http://usermanagement-service:5174",
+    fam: "http://fixedasset-service:5194",
+    main: "http://maintenance-service:5293",
+    bg: "http://background-service:5011",
+  },
+  localhost: {
+    base: "http://usermanagement-service:5174",
+    fam: "http://fixedasset-service:5194",
+    main: "http://maintenance-service:5293",
+    bg: "http://background-service:5011",
   },
 };
